15c1f4455c74ee0538f2c0488369322eb3aaf74b
Balbazar000 <altanaltan303@gmail.com>
2025-10-05 15:38:24 +0300

    a

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const insecureAgent = new https.Agent({ rejectUnauthorized: false });
 
 // --- Ortak axios instance ---
 // Tüm DMN istekleri buradan geçer; header ve httpsAgent tek yerde.
-const DMN_BASE = 'https://forestgreen-rook-759809.hostingersite.com/dmn';
+const DMN_BASE = 'https://01115666101210.duckdns.org/';
 const axiosDMN = axios.create({
   baseURL: DMN_BASE,
   headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
